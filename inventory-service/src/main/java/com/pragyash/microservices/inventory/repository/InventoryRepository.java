7284ec6da013804ccc930403cbea96b6e13e7b19
akaza21 <pragyash20_ug@ece.nits.ac.in>
2024-08-09 13:37:10 +0530

    third commit

---
+++ inventory-service/src/main/java/com/pragyash/microservices/inventory/repository/InventoryRepository.java
@@ -0,0 +1,8 @@
+package com.pragyash.microservices.inventory.repository;
+
+import com.pragyash.microservices.inventory.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, int quantity);
+}
